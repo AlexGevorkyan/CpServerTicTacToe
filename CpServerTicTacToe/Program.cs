944fd071dbc91eb194c7ac7695ad37f3749a0390
Laptop <Laptop@DESKTOP-T33BF5Q>
2022-12-01 09:57:07 +0200

    Add project files.

---
+++ CpServerTicTacToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CpServerTicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
